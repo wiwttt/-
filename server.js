b1b7b07e414b6c0a89b0c03db260381adcbc634e
wiwttt <08.mim357@inbox.ru>
2026-02-24 00:11:10 +0300

    чисти от лишних папок

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet'); 
+
+const app = express();
+const port = 3000;
+
+app.use(helmet());
+app.use(cors()); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log("Example app listening on port ${port}")
-})
+    console.log('Example app listening on port ${port}');
+});
